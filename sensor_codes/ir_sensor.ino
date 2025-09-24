068df36b73f3bb8397206f9469083a5d7526ef6c
Noor-E-Hamim NIbir <150596777+hamim-nibir@users.noreply.github.com>
2025-09-24 12:54:41 +0600

    Update ir_sensor.ino

--- sensor_codes/ir_sensor.ino
+++ sensor_codes/ir_sensor.ino
@@ -1,1 +1,22 @@
 
+#define IR_1 7
+#define IR_2 8 
+#define buzzer 13
+
+void setup() {
+  pinMode(IR_1, INPUT);
+  pinMode(IR_2, INPUT);
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop() {
+  // Read sensor values
+  int ir1State = digitalRead(IR_1);
+  int ir2State = digitalRead(IR_2);
+
+  if (ir1State == LOW) {
+    digitalWrite(buzzer, HIGH);
+  } else {
+    digitalWrite(buzzer, LOW);
+  }
+}
